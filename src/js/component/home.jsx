eb716737555878ea3aa8d75df057f35c873d3524
Yussef Merhi <101605162+Annorakx@users.noreply.github.com>
2024-05-14 16:49:51 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,58 @@
-import React from "react";
+import React, { useState } from 'react'
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tasks, setTasks] = useState(["Make the bed"])
+    const [newTask, setNewTask] = useState("")
+
+	function addTask(e) {
+        if(e.key === "Enter" && newTask.trim() !== "") { // Checkea que la tarea escrita no este vacia y borra los espacios en blanco de antes y despues. Y tambien que se pulse Enter.
+            const trimmedTask = newTask.trim(); // borra los espacios en blanco de antes y despues
+            const capitalizedTask = trimmedTask.charAt(0).toUpperCase() + trimmedTask.slice(1); // chartAt(0).toUpperCase() pone la primera letra en mayuscula y slice(1) añade el resto
+            setTasks([...tasks, capitalizedTask])
+            setNewTask("")
+        }
+    }
+
+	function deleteTask(indexToDelete) {
+        setTasks(tasks.filter((task, index) => index !== indexToDelete))
+    }
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<> 
+			<div className='container'>
+				<h1 className='title'>TODOS</h1>
+				<div className='list-container'>
+					<ul>
+						<li>
+							<input 
+								type="text" 
+								onChange={(e)=>setNewTask(e.target.value)}
+								onKeyDown={addTask}
+								value={newTask}
+								placeholder='What Needs To Be Done?'/>	
+						</li>
+						{tasks.map((task,index) =>      
+                                <li key={index} className="d-flex align-items-center list-task">
+                                    {task}
+                                    <i onClick={()=>deleteTask(index)} className="fa-solid fa-xmark btn fs-2"></i>
+                                </li>
+                            )}
+						<li className="d-flex align-items-center items">
+                                <div className="lines-item"></div>
+                                {tasks.length === 1 ? `${tasks.length} item left` : `${tasks.length} items left`}
+                            </li>
+					</ul>
+                    <ul className="ul-secondary list-container">
+                    <li id="second-paper"></li>
+                    <li id="third-paper"></li>
+                    </ul>
+				</div>
+			</div>
+        </>
 	);
 };
 
